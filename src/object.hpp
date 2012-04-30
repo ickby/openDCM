52bff1a855975845464cc5ab1a9b7df04aa7efbb
Stefan Tröger <stefantroger@gmx.de>
2012-04-30 21:04:11 +0200

    document object class

--- src/object.hpp
+++ src/object.hpp
@@ -52,7 +52,6 @@ template<typename T>
 struct map_key {
     typedef typename T::first type;
 };
-
 template<typename T>
 struct map_val {
     typedef typename T::second type;
@@ -60,16 +59,32 @@ struct map_val {
 
 }
 
-template<typename T>
-void pretty(T t) {
-  std::cout<<__PRETTY_FUNCTION__<<std::endl;
-};
 
+/**
+ * @brief Base class for all object types
+ *
+ * This class add's property and signal capabilitys to all deriving classes. For properties it is tigthly
+ * integrated with the system class: It searches systems property list for the derived class as specified by
+ * the second template parameter and makes it accessible via appopriate functions. Signals are speciefied by a
+ * mpl::map with signal name type as key and a boost::function as values.
+ *
+ * \tparam Sys class of type System of which the properties are taken
+ * \tparam Obj the type of the derived object
+ * \tparam Sig a mpl::map specifing the object's signals by (type -  boost::function) pairs
+ **/
 template<typename Sys, typename Obj, typename Sig>
 struct Object {
 
     Object(Sys& system) : m_system(system) {};
 
+    /**
+      * @brief Access properties
+      *
+      * Returns a reference to the propertys actual value. The property type has to be registerd to the
+      * System type which was given as template parameter to this object.
+      * @tparam Prop property type which should be accessed
+      * @return Prop::type& a reference to the properties actual value.
+      **/
     template<typename Prop>
     typename Prop::type& getProperty() {
         typedef typename mpl::find<Sequence, Prop>::type iterator;
@@ -77,6 +92,15 @@ struct Object {
         return fusion::at<distance>(m_properties);
     };
 
+    /**
+       * @brief Set properties
+       *
+       * Set'S the value of a specified property. The property type has to be registerd to the
+       * System type which was given as template parameter to this object. Note that setProperty(value)
+       * is equivalent to getProperty() = value.
+       * @tparam Prop property type which should be setProperty
+       * @param value value of type Prop::type which should be set in this object
+       **/
     template<typename Prop>
     void setProperty(typename Prop::type value) {
         typedef typename mpl::find<Sequence, Prop>::type iterator;
@@ -84,6 +108,17 @@ struct Object {
         fusion::at<distance>(m_properties) = value;
     };
 
+    /**
+     * @brief Connects a slot to a specified signal.
+     *
+     * Slots are boost::functions which get called when the signal is emitted. Any valid boost::function
+     * which ressembles the signal tyes signature can be registert. It is important that the signal type
+     * was registerd to this object on creation by the appropriate template parameter.
+     *
+     * @tparam S the signal which should be intercepted
+     * @param function boost::function which resembles the signal type's signature
+     * @return void
+     **/
     template<typename S>
     void connectSignal( typename mpl::at<Sig, S>::type function ) {
         typedef typename mpl::find<sig_name, S>::type iterator;
@@ -91,6 +126,16 @@ struct Object {
         fusion::at<distance>(m_signals).push_back(function);
     };
 
+    /**
+    * @brief Disconnects a slot for a specific signal.
+    *
+    * Disconnects a slot so that it dosn't get called at signal emittion. It's important to 
+    * disconnect the slot by the same boost:function it was connected with.
+    * 
+    * @tparam S the signal type of interest
+    * @param function boost::function with which the slot was connected
+    * @return void
+    **/
     template<typename S>
     void disconnectSignal( typename mpl::at<Sig, S>::type function ) {
         typedef typename mpl::find<sig_name, S>::type iterator;
@@ -103,13 +148,21 @@ struct Object {
 
 protected:
 
-    //properties
+    /*properties
+     * search the property map of the system class and get the mpl::vector of properties for the
+     * derived type. It's imortant to not store the properties but their types. These types are
+     * stored and accessed as fusion vector.
+     * */
     typedef typename mpl::at<typename Sys::object_properties, Obj>::type Mapped;
     typedef typename mpl::if_< boost::is_same<Mapped, mpl::void_ >, mpl::vector<>, Mapped>::type Sequence;
     typedef typename mpl::transform<Sequence, details::property_type<mpl::_1> >::type Typesequence;
     typedef typename fusion::result_of::as_vector<Typesequence>::type Properties;
 
-    //signal handling
+    /*signal handling
+     * extract all signal types to allow index search (inex search on signal functions would fail as same 
+     * signatures are supported for multiple signals). Create std::vectors to allow multiple slots per signal
+     * and store these vectors in a fusion::vector for easy access.
+     * */    
     typedef typename mpl::fold< Sig, mpl::vector<>,
     mpl::push_back<mpl::_1, details::map_key<mpl::_2> > >::type sig_name;
     typedef typename mpl::fold< Sig, mpl::vector<>,
@@ -122,7 +175,7 @@ protected:
     Properties m_properties;
     Signals m_signals;
 
-    //with no vararg templates before c++11 we need preprocessor to create the overloads of emit signal we need    
+    //with no vararg templates before c++11 we need preprocessor to create the overloads of emit signal we need
 #define EMIT_ARGUMENTS(z, n, data) \
     BOOST_PP_CAT(data, n)
 
