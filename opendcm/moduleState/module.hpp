d77afb164f450de37b1855575291ea0a584dce42
Stefan Tröger <stefantroger@gmx.de>
2013-02-03 13:21:43 +0100

    finish restructure

---
+++ opendcm/moduleState/module.hpp
@@ -0,0 +1,121 @@
+/*
+    openDCM, dimensional constraint manager
+    Copyright (C) 2013  Stefan Troeger <stefantroeger@gmx.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef DCM_MODULE_STATE_H
+#define DCM_MODULE_STATE_H
+
+#define DCM_USE_PARSER
+
+#include <iosfwd>
+#include <boost/shared_ptr.hpp>
+
+#include <opendcm/core/object.hpp>
+#include <opendcm/core/property.hpp>
+#include <opendcm/core/clustergraph.hpp>
+#include <opendcm/modulePart/module.hpp>
+#include "traits.hpp"
+#include "indent.hpp"
+#include "generator.hpp"
+
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/for_each.hpp>
+#include <boost/fusion/container/vector/convert.hpp>
+#include <boost/fusion/include/as_vector.hpp>
+#include <boost/fusion/include/at.hpp>
+#include <boost/fusion/include/at_c.hpp>
+
+#include <boost/spirit/include/karma.hpp>
+#include <boost/spirit/include/karma_rule.hpp>
+#include <boost/spirit/include/karma_grammar.hpp>
+#include <boost/spirit/include/karma_generate.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+namespace karma = boost::spirit::karma;
+namespace phx = boost::phoenix;
+namespace fusion = boost::fusion;
+
+namespace dcm {
+
+struct ModuleState {
+
+    template<typename Sys>
+    struct type {
+
+
+        template<typename Derived, typename Sig>
+        void generate_properties(std::ostream stream, boost::shared_ptr< dcm::Object<Sys, Derived, Sig> > obj)  {
+
+            typedef typename dcm::Object<Sys, Derived, Sig>::Sequence Properties;
+        };
+
+        struct inheriter {
+
+
+            inheriter() :  m_this((Sys*) this) {}
+            Sys* m_this;
+
+            void saveState(std::ostream& stream) {
+                //typedef std::back_insert_iterator<std::string> iterator_type;
+                typedef std::ostream_iterator<char> iterator_type;
+		typedef typename boost::graph_traits<typename Sys::Cluster>::vertex_iterator viter;
+                
+
+                //std::string s;
+                //std::back_insert_iterator<std::string> out(s);
+		
+		boost::iostreams::filtering_ostream indent_stream;
+                indent_stream.push(indent_filter());
+                indent_stream.push(stream);
+		
+		std::ostream_iterator<char> out(indent_stream);
+
+                std::pair<viter,viter> res = boost::vertices(m_this->m_cluster);
+                boost::iterator_range<viter> range(res.first,res.second);
+		generator<iterator_type, Sys> gen(*m_this);
+
+                karma::generate(out, gen, range);
+
+                
+                //indent_stream << s;
+
+            };
+
+            void loadState(std::istream& stream) {
+
+            };
+        };
+
+
+        //nothing to add to the objects and properties of other modules
+        typedef mpl::vector0<>  properties;
+        typedef mpl::vector0<>  objects;
+
+        //nothing to do on startup
+        static void system_init(Sys& sys) {};
+    };
+};
+
+}
+
+#endif //DCM_MODULE_STATE_H
+
+
+
+
+
